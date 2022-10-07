b423af69bda2f5c53ec882d77fd92e577bf3406b
Chris <chrisk@cityc.co.uk>
2022-10-07 16:16:03 +0100

    version 0.0.8

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-      .package(url: "git@github.com:ChrisKlimpke/TrackerSDKv2.git", from: "1.0.0")
+      .package(url: "https://github.com/ChrisKlimpke/TrackerSDKv2.git", from: "1.1.10")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -23,3 +23,5 @@ let package = Package(
                   path: "VykWKWebView.xcframework"),
     ]
 )
+
+
